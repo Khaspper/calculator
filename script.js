9c7d93edb89f0fbbc93b70fa42aa039ebd401b2d
Markus <brandonnarciso24@gmail.com>
2023-08-08 21:28:20 -0700

    Add a operate function
    the computer can now call the math function

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, operator, secondNumber) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(firstNumber, secondNumber);
+            break;
+        case '-':
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            result = divide(firstNumber, secondNumber);
+            break;
+    
+        default:
+            break;
+    }
+    return result;
+}
+
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
